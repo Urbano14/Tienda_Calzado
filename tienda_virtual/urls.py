a5439fc07f7d5f6e48f1653caf343163c8f62c49
Urbano14 <urbanoblanesbonachera@gmail.com>
2025-11-26 19:34:03 +0100

    Fix: servir ficheros MEDIA en producción

--- tienda_virtual/urls.py
+++ tienda_virtual/urls.py
@@ -1,40 +1,27 @@
-"""
-URL configuration for tienda project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
+from django.urls import path, include, re_path
+
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path, include
+from django.views.static import serve
 
-# Aqui se anyaden los path de las urls de cada app en nombreapp/templates/nombreapp
 urlpatterns = [
-    path('api/', include('clientes.urls')),
-    path('carrito/', include('carrito.urls')),
-    path('', include('productos.urls')),
-    path('', include('pedidos.urls')),
     path('admin/', admin.site.urls),
+    path('', include('productos.urls')),
+    path('carrito/', include('carrito.urls')),
+    path('clientes/', include('clientes.urls')),
+    path('pedidos/', include('pedidos.urls')),
 ]
 
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+# Servir MEDIA y archivos subidos por usuarios
+# --------------------------------------------------------
+# En local (DEBUG=True):
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# En producción (Render, DEBUG=False):
+# Render NO sirve media automáticamente, así que usamos una vista estática explícita.
 else:
-    # En despliegues simples sin servidor web dedicado a media, exponemos media/ con Django.
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
